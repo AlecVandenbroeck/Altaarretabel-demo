707c6157e6eb4bbd238aeeefab41c74ec1df1741
Alec Van den broeck <46535484+AlecVandenbroeck@users.noreply.github.com>
2021-10-11 10:57:27 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+var mirador = Mirador.viewer({
+  "id": "my-mirador",
+  "manifests": {
+    "http://127.0.0.1:8887/base_manifest.json": {
+      "provider": "KMSKA"
+    },
+    "http://127.0.0.1:8887/manifest_layers_v2.json": {
+      "provider": "IIIF Fridays"
+    }
+  },
+  "windows": [
+    {
+      "loadedManifest": "http://127.0.0.1:8887/Lam_gods_manifest.json",
+      "canvasIndex": 0,
+      "highlightAllAnnotations": true,
+      "sideBarPanel": 'annotations',
+      "sideBarOpen": true,
+      "thumbnailNavigationPosition": 'far-bottom'
+    }
+  ],
+  "language": "nl",
+  "annotations": {
+    "htmlSanitizationRuleSet": 'mirador2', // See src/lib/htmlRules.js for acceptable values
+  }
+});
---
+++ plugin.js
@@ -0,0 +1,20 @@
+let selectedAnnotation = null;
+let focusedWindow = null;
+var listener = () => {
+	state = mirador.store.getState();
+	if ('selectedAnnotationId' in state.windows[state.workspace.focusedWindowId] && typeof state.windows[state.workspace.focusedWindowId].selectedAnnotationId !== 'undefined' && focusedWindow === state.workspace.focusedWindowId && selectedAnnotation !== state.windows[state.workspace.focusedWindowId].selectedAnnotationId) {
+		t = state.windows[state.workspace.focusedWindowId].selectedAnnotationId.split('/')
+		mirador.store.dispatch(Mirador.actions.deselectAnnotation(focusedWindow, state.windows[state.workspace.focusedWindowId].selectedAnnotationId));
+		mirador.store.dispatch(Mirador.actions.addWindow({manifestId: 'http://127.0.0.1:8887/Lam_gods_manifest.json'}));
+		mirador.store.dispatch(Mirador.actions.setCanvas(state.workspace.focusedWindowId ,'http://127.0.0.1:8887/canvas/p' + t[t.length -1]));
+	}
+	focusedWindow = state.workspace.focusedWindowId;
+	if ('selectedAnnotationId' in state.windows[state.workspace.focusedWindowId]) {
+		selectedAnnotation = state.windows[state.workspace.focusedWindowId].selectedAnnotationId;
+	}
+	else {
+		selectedAnnotation = null;
+	}
+};
+
+mirador.store.subscribe(listener);
